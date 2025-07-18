aec3ec30adeb7a7955ddd5bee09aff135b630fc6
Endrococsx <mafrakaique9@gmail.com>
2025-07-18 11:09:30 -0300

    Update questao01.c

--- questao01.c
+++ questao01.c
@@ -1,18 +1,20 @@
-#include <stdio.h> 
-int main(){ 
-     int i, a; 
-    int negativos = 0; 
- 
-    for (i = 1; i <= 5; i++) { 
-        printf("Digite o valor %d: ", i); 
-        scanf("%d", &a); 
- 
-        if (a < 0) { 
-            negativos++; 
-        } 
-    } 
- 
-    printf("Quantidade de valores negativos: %d\n", negativos); 
- 
-    return 0; 
-} 
+#include <stdio.h>
+int main(){
+
+printf("Escreva cinco valoras para a:\n");
+int i;
+int a;
+int soma = 0;
+
+for(i = 1; i <= 5; i++){
+   scanf("%d", &a);
+   if(a < 0){
+      soma += 1;
+   }
+}
+
+printf("há um total de %d números negativos", soma);
+
+
+return 0;
+}
