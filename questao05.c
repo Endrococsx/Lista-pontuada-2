030fb1c8564e8029ef25c3cac625c49d3a6b7fa9
Endrococsx <mafrakaique9@gmail.com>
2025-07-18 13:22:57 -0300

    Update questao05.c

--- questao05.c
+++ questao05.c
@@ -1,23 +1,24 @@
-#include <stdio.h> 
-int main(){ 
-    int valor; 
-    int soma = 0; 
-    int quantidade = 0; 
-    printf("Digite valores inteiros positivos (um valor negativo para encerrar):\n"); 
-    while (1) { 
-        scanf("%d", &valor); 
-        if (valor < 0) { 
-            break; 
-        } 
-        soma += valor; 
-        quantidade++; 
-    } 
-    if (quantidade == 0) { 
-        printf("Nenhum valor positivo foi inserido.\n"); 
-    } else { 
-        float media = (float)soma / quantidade; 
-        printf("Média aritmética: %.2f\n", media); 
-    } 
- 
-    return 0; 
+#include <stdio.h>
+int main (){
+
+printf("Escreva valores inteiros positivos para a contagem:\n");
+float a;
+float contagem = 0;
+float soma = 0;
+
+do{
+   scanf("%f", &a);
+   if (a < 0){
+      break;
+   }else if(a > 0){
+      contagem += 1;
+      soma += a;
+   }
+
+}while(1);
+
+float media = soma / contagem;
+printf("A média aritmética dos números inseridos É:\n%.2f", media);
+
+return 0;
 }
